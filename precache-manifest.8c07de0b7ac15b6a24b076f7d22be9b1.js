54a324cd9868bedfe7117aa9822207b938cb0226
James <ranmaru520000@gmail.com>
2020-05-31 13:16:02 +0900

    Updates

---
+++ precache-manifest.8c07de0b7ac15b6a24b076f7d22be9b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2313ebce040e49be1ebc100a6cf0f704",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "42e814a2dcc035539c4e",
+    "url": "/monsters-rolodex/static/css/main.3d9612db.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42e814a2dcc035539c4e",
+    "url": "/monsters-rolodex/static/js/main.d4ed36d4.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
